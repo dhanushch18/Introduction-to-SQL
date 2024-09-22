f9362f9b09f4d58a9977414554b21d8c0aeabb6b
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-09-22 22:14:42 +0530

    Create 07HackerRank.sql

---
+++ 07HackerRank.sql
@@ -0,0 +1,38 @@
+Query a list of CITY and STATE from the STATION table.
+The STATION table is described as follows: 
+
+select CITY,STATE from STATION;
+
+
+Congratulations!
+
+You have passed the sample test cases. Click the submit button to run your code against all the test cases.
+Your Output (stdout)
+
+    Kissee Mills MO
+
+    Loma Mar CA
+
+    Sandy Hook CT
+
+    Tipton IN
+
+    Arlington CO
+
+    Turner AR
+
+    Slidell LA
+
+    Negreet LA
+
+    Glencoe KY
+
+    Chelsea IA
+
+    Chignik Lagoon AK
+
+    Pelahatchie MS
+
+    Hanna City IL
+
+    Dorrance KS
