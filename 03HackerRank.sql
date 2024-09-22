c72d407def3183271de733452d3265197e778070
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-09-22 21:29:12 +0530

    Create 03HackerRank.sql

---
+++ 03HackerRank.sql
@@ -0,0 +1,28 @@
+Query all columns (attributes) for every row in the CITY table.
+
+The CITY table is described as follows: 
+
+
+  select * from CITY;
+
+Compiler Message
+
+Success
+
+Input (stdin)
+
+     
+
+Expected Output
+
+    6 Rotterdam NLD Zuid-Holland 593321 
+
+    3878 Scottsdale USA Arizona 202705 
+
+    3965 Corona USA California 124966 
+
+    3973 Concord USA California 121780 
+
+    3977 Cedar Rapids USA Iowa 120758 
+
+    3982 Coral Springs USA Florida 117549 
