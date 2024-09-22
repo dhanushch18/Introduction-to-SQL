62e7c69ace72d29886abaa713dfd176f541ad46c
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-09-22 21:49:20 +0530

    Create 04HackerRank.sql

---
+++ 04HackerRank.sql
@@ -0,0 +1,14 @@
+Query all columns for a city in CITY with the ID 1661.
+
+The CITY table is described as follows: 
+
+
+select * from CITY where ID="1661";
+
+
+Congratulations!
+
+You have passed the sample test cases. Click the submit button to run your code against all the test cases.
+Your Output (stdout)
+
+    1661 Sayama JPN Saitama 162472
