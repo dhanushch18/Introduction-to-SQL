a84840674f4acf8aa3eab0a93003997667fcdc0f
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-09-22 21:51:25 +0530

    Create 05HackerRank.sql

---
+++ 05HackerRank.sql
@@ -0,0 +1,25 @@
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN. 
+
+
+select * from CITY where COUNTRYCODE="JPN";
+
+
+Compiler Message
+
+Success
+
+Input (stdin)
+
+     
+
+Expected Output
+
+    1613 Neyagawa JPN Osaka 257315 
+
+    1630 Ageo JPN Saitama 209442 
+
+    1661 Sayama JPN Saitama 162472 
+
+    1681 Omuta JPN Fukuoka 142889 
+
+    1739 Tokuyama JPN Yamaguchi 107078 
