aa7d1006d5b452e8542b2c380a177d473e1ed1aa
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-09-22 22:11:32 +0530

    Create 06HackerRank.sql

---
+++ 06HackerRank.sql
@@ -0,0 +1,21 @@
+Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+The CITY table is described as follows: 
+
+
+select NAME from CITY where COUNTRYCODE="JPN";
+
+
+Congratulations!
+
+You have passed the sample test cases. Click the submit button to run your code against all the test cases.
+Your Output (stdout)
+
+    Neyagawa
+
+    Ageo
+
+    Sayama
+
+    Omuta
+
+    Tokuyama
